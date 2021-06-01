062650e8afd449aa185fa4895cff85abce95a632
yangqisong <47423485+yangqisong@users.noreply.github.com>
2021-06-01 22:21:04 +0200

    Update mpi_tf.py

--- wcsac/utils/mpi_tf.py
+++ wcsac/utils/mpi_tf.py
@@ -1,7 +1,7 @@
 import numpy as np
 import tensorflow as tf
 from mpi4py import MPI
-from safe_rl.utils.mpi_tools import broadcast
+from wc_sac.utils.mpi_tools import broadcast
 
 
 def flat_concat(xs):
