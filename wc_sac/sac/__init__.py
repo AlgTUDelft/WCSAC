485c2ab0ce795c01005158eaee4e6ef106603a82
qisong-yang <47423485+qisong-yang@users.noreply.github.com>
2021-11-19 14:47:57 +0100

    Update __init__.py

--- wc_sac/sac/__init__.py
+++ wc_sac/sac/__init__.py
@@ -1,1 +1,10 @@
+from gym.envs.registration import register
+register(
+    id='SPYENV-v0',
+    entry_point='wcsac.sac.SpyUnimodal:SpyUnimodal',
+)
 
+register(
+    id='SPYENV-v1',
+    entry_point='wcsacsac.sac.SpyBimodal:SpyBimodal',
+)
